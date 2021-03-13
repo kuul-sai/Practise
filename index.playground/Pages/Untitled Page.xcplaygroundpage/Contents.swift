3fb271c4676343e917a3277d5939418b013df83b
Samanth Sai <samanthsai@Samanths-MacBook-Pro.local>
2021-03-13 12:10:58 +0530

    First time

---
+++ index.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+var str = "Hello, playground"
+var firstname = "samanth"
+print(firstname)
+print(str)
+let mrp = 500
+var num:Int = 600
+var hel = 600
+print("The given value is \(num) and \(hel)")
+var i:Float = 3.178
+var k:Double = 3.178
+print("\(i) and \n \(k)")
+var sam = "samanth sai challa"
